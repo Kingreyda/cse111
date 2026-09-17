85106797743ed516734ccacd549666be0cabeda7
King David Olaribigbe <olaribigbeking@gmail.com>
2026-09-17 15:08:43 +0100

    User-defined functions

---
+++ week02/calling_functions.py
@@ -0,0 +1,18 @@
+def calculate_age_number(prompt):
+    """
+    Asks user for age and add 10 years to the age number based on the given age.
+
+    Parameters:
+    age (int): The age of the person.
+
+    Returns:
+    int: The calculated age number.
+    """
+    value = int(input(prompt))
+    while value < 5:
+        print("Please enter an age above 5")
+        value = int(input(prompt))
+    return value + 10
+
+age = calculate_age_number("\nWhat is your age? ")
+print(f"Your age in 10 years will be: {age}")
