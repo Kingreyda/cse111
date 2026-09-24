bae24fdbde3a35d70316ff16bdecee18d9290330
King David Olaribigbe <olaribigbeking@gmail.com>
2026-09-24 12:08:53 +0100

    Implement and validate water flow calculation tests

--- week02/calling_functions.py
+++ week02/calling_functions.py
@@ -1,15 +1,43 @@
-def calculate_age_number(prompt):
-    """
-    Asks user for age and add 10 years to the age number based on the given age.
+"""Compute and print the volume of a right circular cone."""
 
-    Returns:
-    int: The calculated age number.
-    """
-    value = int(input(prompt))
-    while value < 5:
-        print("Please enter an age above 5")
-        value = int(input(prompt))
-    return value + 10
+# Import the standard math module so that
+# math.pi can be used in this program.
+import math
 
-age = calculate_age_number("\nWhat is your age? ")
-print(f"Your age in 10 years will be: {age}")
+def main():
+
+  # Call the cone_volume function to compute
+  # the volume of an example cone.
+  ex_radius = 2.8
+  ex_height = 3.2
+  ex_vol = cone_volume(ex_radius, ex_height)
+
+  # Print several lines that describe this program.
+  print("This program computes the volume of a right")
+  print("circular cone. For example, if the radius of a")
+  print(f"cone is {ex_radius} and the height is {ex_height}")
+  print(f"then the volume is {ex_vol:.1f}")
+  print()
+
+  # Get the radius and height of the cone from the user.
+  radius = float(input("Please enter the radius of the cone: "))
+  height = float(input("Please enter the height of the cone: "))
+
+  # Call the cone_volume function to compute the volume
+  # for the radius and height that came from the user.
+  vol = cone_volume(radius, height)
+
+  # Print the radius, height, and
+  # volume for the user to see.
+  print(f"Radius: {radius}")
+  print(f"Height: {height}")
+  print(f"Volume: {vol:.1f}")
+
+def cone_volume(radius, height):
+  """Compute and return the volume of a right circular cone."""
+  volume = math.pi * radius**2 * height / 3
+  return volume
+
+# Start this program by
+# calling the main function.
+main()
